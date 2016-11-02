2347a88b40f4f342be24f0cd467bcebdde768867
Yury Prokashev <yury.prokashev@gmail.com>
2016-11-02 08:19:10 +0300

    added secure pusher listener

--- pusherjs.js
+++ pusherjs.js
@@ -6,5 +6,9 @@ const PusherService = require('./src/PusherService2');
 const express = require('express');
 const server = express();
 
-var app = new PusherService(server);
+var app = new PusherService(server, 'http');
 app.listen();
+
+const serverSecure = express();
+var appSecure = new PusherService(serverSecure, 'https');
+appSecure.listen();
